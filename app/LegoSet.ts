2b05cd3397efb1b74a1f0a16cd3e3e007dddaf23
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-01-25 14:00:06 +0100

    initial lego set model added, first interpolation used

---
+++ app/LegoSet.ts
@@ -0,0 +1,13 @@
+export class LegoSet {
+    id: number;
+    version: number;
+    externalId: string;
+    name: string;
+    status: string;
+    comment: string
+}
+
+export enum Status {
+    Used,
+    New
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,26 @@
-import {Component} from '@angular/core';
+import { LegoSet, Status } from './LegoSet';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: '<div>Hello World</div>',
+  template: `<h1>{{appTitle}}</h1>
+      <h2>{{legoSet.name}} Lego Set Details</h2>
+      <div><label>Id: </label>{{legoSet.id}}</div>
+      <div><label>ExternalId: </label>{{legoSet.externalId}}</div>
+      <div><label>version: </label>{{legoSet.version}}</div>
+      <div><label>Name: </label>{{legoSet.name}}</div>
+      <div><label>Status: </label>{{legoSet.status}}</div>
+      <div><label>Comment: </label>{{legoSet.comment}}</div>
+  `,
 } as Component)
 export class AppComponent {
+  appTitle: string = 'Lego Angular2 App';
+  legoSet: LegoSet = {
+    id: 1,
+    externalId: 'd99ae2c4-e2fa-11e6-bf01-fe55135034f3',
+    version: 1,
+    name: 'set name',
+    status: Status[Status.New],
+    comment: 'N/A'
+  }
 }
