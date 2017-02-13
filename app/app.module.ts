5c66ae16fceab03d98e8bbe4281bae7e56fe4a98
rbuchals <robert.buchalski@gmail.com>
2017-02-13 15:49:13 +0100

    New component lego-shop. See site map for details.;

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { LegoModule } from './lego/lego.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LegoShopComponent } from './lego-shop/legoShop.component';
 
 @NgModule({
   imports: [BrowserModule, LegoModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, LegoShopComponent],
   bootstrap: [AppComponent],
   providers: []
 })
