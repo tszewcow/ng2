c84dc225b9506377c3e988349fe5bb9741626862
rbuchals <robert.buchalski@gmail.com>
2017-02-14 22:37:13 +0100

    Searching on start site + Replace ngOnInit -> Observable.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed: boolean = true;
+  currentSearchPhrase: string;
+
+  constructor(private router: Router) { }
+
+  search(): void {
+        if (this.currentSearchPhrase) {
+            this.router.navigate(['lego-shop', this.currentSearchPhrase]);
+        } else {
+            this.router.navigate(['lego-shop']);
+        }
+    }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,10 @@ import { LegoShopModule } from './lego-shop/legoShop.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [BrowserModule, LegoModule, LegoShopModule, AppRoutingModule],
+  imports: [BrowserModule, LegoModule, LegoShopModule, AppRoutingModule, FormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   providers: []
