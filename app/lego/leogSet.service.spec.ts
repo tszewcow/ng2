e9a70fd3825755499d852c2e6676b26a51f500d7
Szewcow Tomasz <tomasz.szewcow.ext@adcubum.com>
2017-02-19 12:47:09 +0100

    unit tests for legoSet.service

---
+++ app/lego/leogSet.service.spec.ts
@@ -0,0 +1,153 @@
+import { LegoSet, Status } from './LegoSet';
+import { MockBackend } from '@angular/http/testing';
+import { LegoSetService } from './legoSet.service';
+import { HttpModule, Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
+import { TestBed, inject } from '@angular/core/testing';
+
+
+describe('legoSet Service tests', () => {
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpModule],
+            providers: [{
+                provide: Http,
+                useFactory: (mockBackend: MockBackend, options: BaseRequestOptions) => {
+                    return new Http(mockBackend, options);
+                },
+                deps: [MockBackend, BaseRequestOptions]
+            },
+                MockBackend,
+                BaseRequestOptions,
+                LegoSetService]
+        });
+    });
+
+    it('should get lego sets', inject([LegoSetService, MockBackend], (legoService: LegoSetService, mockBackend: MockBackend) => {
+
+        // given
+        const mockedResponse: LegoSet[] = [
+            {
+                id: 0,
+                version: 0,
+                externalId: '123',
+                name: 'set1',
+                status: Status[Status.New],
+                comment: 'comment',
+                imagePath: 'pathToImage'
+            }
+        ];
+
+        mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(mockedResponse)
+            })));
+        });
+
+        // when
+        legoService.getLegoSetsHttp().subscribe((legoSets) => {
+            // then
+            expect(legoSets.length).toBe(1);
+            expect(legoSets[0].id).toBe(0);
+            expect(legoSets[0].name).toBe('set1');
+        });
+    }));
+
+    it('should get lego set', inject([LegoSetService, MockBackend], (legoService: LegoSetService, mockBackend: MockBackend) => {
+
+        // given
+        const mockedResponse: LegoSet = {
+            id: 0,
+            version: 0,
+            externalId: '123',
+            name: 'set1',
+            status: Status[Status.New],
+            comment: 'comment',
+            imagePath: 'pathToImage'
+        };
+        const id = 0;
+
+        mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(mockedResponse)
+            })));
+        });
+
+        // when
+        legoService.findOneHttp(id).subscribe((legoSet) => {
+            // then
+            expect(legoSet.id).toBe(id);
+            expect(legoSet.name).toBe('set1');
+        });
+    }));
+
+    it('should add lego set', inject([LegoSetService, MockBackend], (legoService: LegoSetService, mockBackend: MockBackend) => {
+
+        // given
+        const requestBody: LegoSet = {
+            id: 0,
+            version: 0,
+            externalId: '123',
+            name: 'set1',
+            status: Status[Status.New],
+            comment: 'comment',
+            imagePath: 'pathToImage'
+        };
+
+        mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                status: 200
+            })));
+        });
+
+        // when
+        legoService.addHttp(requestBody).subscribe((response) => {
+            // then
+            expect(response.status).toBe(200);
+        });
+    }));
+
+    it('should update lego set', inject([LegoSetService, MockBackend], (legoService: LegoSetService, mockBackend: MockBackend) => {
+
+        // given
+        const requestBody: LegoSet = {
+            id: 0,
+            version: 0,
+            externalId: '123',
+            name: 'set1',
+            status: Status[Status.New],
+            comment: 'comment',
+            imagePath: 'pathToImage'
+        };
+
+        mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                status: 200
+            })));
+        });
+
+        // when
+        legoService.editHttp(requestBody).subscribe((response) => {
+            // then
+            expect(response.status).toBe(200);
+        });
+    }));
+
+    it('should delete lego set', inject([LegoSetService, MockBackend], (legoService: LegoSetService, mockBackend: MockBackend) => {
+
+        // given
+        const id = 0;
+
+        mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                status: 200
+            })));
+        });
+
+        // when
+        legoService.deleteHttp(id).subscribe((response) => {
+            // then
+            expect(response.status).toBe(200);
+        });
+    }));
+});
